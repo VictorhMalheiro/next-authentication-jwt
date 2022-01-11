01feae3e35703f2b9c3a75f764896f575336eb0d
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2022-01-10 22:40:26 -0300

    Add MUI5

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,140 +1,140 @@
 import { createContext, ReactNode, useState, useEffect } from "react";
-import { setCookie, parseCookies } from'nookies'
+import { setCookie, parseCookies } from 'nookies'
 import { ToastContainer, toast } from 'react-toastify';
 import Router from "next/router";
 
 import { api } from "../services/api";
 
 type User = {
-    email: string;
-    permissions: string[];
-    roles: string[];
+	email: string;
+	permissions: string[];
+	roles: string[];
 }
 
 type SignInCredentials = {
-    email: string;
-    password: string;
+	email: string;
+	password: string;
 }
 
 type AuthContextData = {
-    signIn(credentials: SignInCredentials): Promise<void>;
-    user: User;
-    isAuthenticated: boolean;
+	signIn(credentials: SignInCredentials): Promise<void>;
+	user: User;
+	isAuthenticated: boolean;
 };
 
 type AuthProviderProps = {
-    children: ReactNode;
+	children: ReactNode;
 }
 
 export const AuthContext = createContext({} as AuthContextData);
 
-export function AuthProvider({children}: AuthProviderProps) {
-    const [user, setUser] = useState<User>();
-    const isAuthenticated = !!user;
+export function AuthProvider({ children }: AuthProviderProps) {
+	const [user, setUser] = useState<User>();
+	const isAuthenticated = !!user;
 
-    useEffect(() => {
-        const { 'nextauth.token': token} = parseCookies();
+	useEffect(() => {
+		const { 'nextauth.token': token } = parseCookies();
 
-        if (token) {
-            api.get('/me').then(response => {
-                const { email, permissions, roles } = response.data;
+		if (token) {
+			api.get('/me').then(response => {
+				const { email, permissions, roles } = response.data;
 
-                setUser({ email, permissions, roles });
-            });
-        }
-    }, []);
+				setUser({ email, permissions, roles });
+			});
+		}
+	}, []);
 
-    async function signIn({email, password}: SignInCredentials) {
-        try {
-            const response = await api.post('/sessions', {
-                email,
-                password
-            })
+	async function signIn({ email, password }: SignInCredentials) {
+		try {
+			const response = await api.post('/sessions', {
+				email,
+				password
+			})
 
-            const { token, refreshToken, permissions, roles } = response.data;
+			const { token, refreshToken, permissions, roles } = response.data;
 
-            setCookie(undefined, 'nextauth.token', token, { 
-                maxAge: 60 * 60 * 24 * 30,
-                path: '/'
-            });
-            setCookie(undefined, 'nextauth.refreshToken', refreshToken, { 
-                maxAge: 60 * 60 * 24 * 30,
-                path: '/'
-            });
+			setCookie(undefined, 'nextauth.token', token, {
+				maxAge: 60 * 60 * 24 * 30,
+				path: '/'
+			});
+			setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+				maxAge: 60 * 60 * 24 * 30,
+				path: '/'
+			});
 
-            setUser({
-                email,
-                permissions,
-                roles
-            })
+			setUser({
+				email,
+				permissions,
+				roles
+			})
 
-            toast.success(`😎 Deu certo! Seja bem vindo:  ${email}}`, {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            });
+			toast.success(`😎 Deu certo! Seja bem vindo:  ${email}}`, {
+				position: "top-right",
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+			});
 
-            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+			api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
-            Router.push('/dashboard');
-        } catch (error) {
-            console.log(error)
+			Router.push('/dashboard');
+		} catch (error) {
+			console.log(error)
 
-            if(error.response.status === 401) {
-                toast.warn('😔 Ohhh no!! Login ou senha incorretos!', {
-                    position: "top-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    });
-                console.log(error)
-            } else if (error.response.status === 404) {
-                toast.warn('😔 Ohhh no!! Usuário não encontrado!', {
-                    position: "top-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    });
-            } else if (error.response.status === 403) {
-                toast.warn('😔 Ohhh no!! Você não tem permissão!', {
-                    position: "top-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    });
-                console.log(error)
-            } else if (error.response.status === 500) {
-                toast.error('😔 Ohhh no!! Algo deu errado!', {
-                    position: "top-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    });
-                console.log(error)
-            }
+			if (error.response.status === 401) {
+				toast.warn('😔 Ohhh no!! Login ou senha incorretos!', {
+					position: "top-center",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				});
+				console.log(error)
+			} else if (error.response.status === 404) {
+				toast.warn('😔 Ohhh no!! Usuário não encontrado!', {
+					position: "top-center",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				});
+			} else if (error.response.status === 403) {
+				toast.warn('😔 Ohhh no!! Você não tem permissão!', {
+					position: "top-center",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				});
+				console.log(error)
+			} else if (error.response.status === 500) {
+				toast.error('😔 Ohhh no!! Algo deu errado!', {
+					position: "top-center",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				});
+				console.log(error)
+			}
 
-        }
-    }
+		}
+	}
 
-    return (
-        <AuthContext.Provider value={{signIn, isAuthenticated, user}}>
-            {children}
-        </AuthContext.Provider>
-    )
+	return (
+		<AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+			{children}
+		</AuthContext.Provider>
+	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,73 @@
-import React, {FormEvent, useContext, useState} from 'react';
+import React, { FormEvent, useContext, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { ToastContainer } from 'react-toastify';
+
+import Box from '@mui/material/Box';
+import { InputText } from '../src/components/InputText';
+import InputLabel from '@mui/material/InputLabel';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
 
 import 'react-toastify/dist/ReactToastify.css';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  const {signIn} = useContext(AuthContext)
+	const { signIn } = useContext(AuthContext)
 
-  async function handleSubmit(event: FormEvent) {
-    event.preventDefault();
+	async function handleSubmit(event: FormEvent) {
+		event.preventDefault();
 
-    const data = {
-      email,
-      password
-    }
+		const data = {
+			email,
+			password
+		}
 
-    await signIn(data);
-  }
+		await signIn(data);
+	}
 
-  return (
-    <div className={styles.main}>
-        <form onSubmit={handleSubmit} className={styles.container}>
-          <label className={styles.label} htmlFor="email">Email
-            <input placeholder='exemple@exemple.com' className={styles.input} type="email" value={email} onChange={e => setEmail(e.target.value)} name="email" id="" />
-          </label>
-          <label className={styles.label} htmlFor="password">Senha
-            <input placeholder='Digite sua senha' className={styles.input} type="password" value={password} onChange={e => setPassword(e.target.value)} name="password" id="" />
-          </label>
-          <button className={styles.button} type="submit">Entrar</button>
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            />
-            {/* Same as */}
-          <ToastContainer />
-        </form>
-    </div>
-  )
+	return (
+		<div className={styles.main}>
+			<Box
+				component="form"
+				noValidate
+				sx={{
+					display: 'grid',
+					gridTemplateColumns: { sm: '1fr 1fr' },
+					gap: 2,
+				}}
+				onSubmit={handleSubmit}
+			>
+				<FormControl variant="standard">
+					<InputLabel shrink htmlFor="bootstrap-input">
+						Email
+					</InputLabel>
+					<InputText value={email} onChange={e => setEmail(e.target.value)} name="email" placeholder="exemple@exemple.com" id="email-input" />
+				</FormControl>
+				<FormControl variant="standard">
+					<InputLabel shrink htmlFor="bootstrap-input">
+						Password
+					</InputLabel>
+					<InputText type="password" value={password} onChange={e => setPassword(e.target.value)} name="password" placeholder="Digite a sua senha" id="password-input" />
+				</FormControl>
+				<Button size='large' variant="contained" type="submit">Entrar</Button>
+				<ToastContainer
+					position="top-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+				/>
+				<ToastContainer />
+			</Box>
+		</div>
+	)
 }
 
 
